72b13a69b41877298cff705166497deaf021ca91
tfermoso <tomas.fermoso@gmail.com>
2023-05-03 16:33:02 +0200

    recogiendo datos para calcular precio ventana

--- static/js/index.js
+++ static/js/index.js
@@ -3,4 +3,14 @@ window.onload=()=>{
         let tipo=e.currentTarget.value;
         document.getElementById("imagenventana").src="/static/img/"+tipo;
     }
+
+    $(".datos").change(()=>{
+        let ancho=$("#ancho").val();
+        let alto=$("#alto").val();
+        let tipo=$("#tipoventana").val();
+        if(ancho!="" && alto!=""){
+            alert("calculando..");            
+        }
+    })
+
 }
