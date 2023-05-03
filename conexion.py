7126f6f3e235beffa61679768f037ed083615552
tfermoso <tomas.fermoso@gmail.com>
2023-05-03 17:23:43 +0200

    creando conexion a mysql

---
+++ conexion.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+
+def select(consulta):
+    # Conectarse a la base de datos
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="ventanas"
+    )
+
+    # Crear un cursor
+    cursor = conexion.cursor()
+
+    # Ejecutar una consulta
+    cursor.execute(consulta)
+
+    # Obtener los resultados de la consulta
+    resultados = cursor.fetchall()
+
+    # Cerrar el cursor y la conexión
+    cursor.close()
+    conexion.close()
+
+    return resultados
